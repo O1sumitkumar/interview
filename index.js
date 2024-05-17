8ba1de7efc00f03e77e7487cd2a832779ffe48ac
O1sumitkumar <SUMITJHA365@gmail.com>
2024-05-17 09:12:45 +0530

    solve some basic questions

--- index.js
+++ index.js
@@ -33,20 +33,20 @@ console.log("I have been started!!!!");
 // console.log(...b);
 //* this will spread the value
 
-// for simple array and object destructuring
+//*5 for simple array and object destructuring
 let users = ["Ram", "Jon"];
 let [user1, user2] = users;
 
 // console.warn(user1);
 // console.warn(user2);
 
-// using split
+//*6 using split
 
 // let [firstName, lastName] = "Sumit jha".split(" ");
 // console.log(firstName);
 // console.log(lastName);
 
-// for nested array and object destructuring
+//*7 for nested array and object destructuring
 // let options = {
 //   size: {
 //     width: 100,
@@ -66,18 +66,112 @@ let [user1, user2] = users;
 // console.log(height);
 // console.warn(title);
 
-// skipping values with arrays
+//*8 skipping values with arrays
 
 // let data = ["Subbu", "ankit", "sumit"];
 // let [dog, , elder] = data;
 // console.log(dog);
 // console.log(elder);
 
-// skipping values with objects
+//*9 skipping values with objects
 
-let data = { dog: "subbu", younger: "ankit", elder: "sumit" };
+// let data = { dog: "subbu", younger: "ankit", elder: "sumit" };
 
-let { younger, dog } = data;
+// let { younger, dog } = data;
 
-console.log(dog);
-console.log(younger);
+// console.log(dog);
+// console.log(younger);
+//* 10
+
+// const name = "Subbu";
+// console.warn(name());
+
+//*11
+// const result = false || {} || null
+
+// console.warn(result)
+
+//* 12
+
+// const result = null || false || "";
+
+// console.warn(result);
+
+//*13
+// const result = [] || 0 || true
+// console.warn(result)
+
+//*14
+
+// console.log(Promise.resolve(5));
+
+//*15
+
+// JSON.parse()
+
+//*16
+
+// let name = "sumit";
+
+// function getName() {
+//   console.log(name);
+//   let name = "subbu";
+// }
+// getName()
+
+//*17
+
+// let name = "sumit";
+
+// function getName() {
+//   console.log(name);
+// }
+
+// getName();
+
+//*18
+// console.log(`${((x) => x)("i  love ")} to program`);
+
+//*19
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(sum(...[1, 2, 3]));
+
+//*20
+// const name = "subbu";
+
+// console.log(!typeof name === "object");
+// console.log(!typeof name === "string");
+// console.log(!typeof name === false);
+
+//*21
+
+// const name = "Jhon";
+// const age = 25;
+
+// console.log(isNaN(name));
+// console.log(isNaN(age));
+
+//*22
+// let user = { name: "subbu", age: 25 };
+// Object.seal(user);
+// user.gender = "male";
+
+// console.log(user);
+
+//*23
+// let data = [1,2,3,4]
+
+// data.shift()
+// console.log(data);
+
+//*24
+// let data = [1, 2, 3, 4];
+// delete data[0]
+// console.log(data);
+
+//*25
+// let a = 31;
+// console.log(a % 2);
